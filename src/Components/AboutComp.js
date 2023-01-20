e62d11bc219d7bdebfc86e3a04429545f785f1c5
Collins Nana Antwi <nanastef10@hotmail.com>
2023-01-20 21:13:54 +0000

    Add size ti image

--- src/Components/AboutComp.js
+++ src/Components/AboutComp.js
@@ -17,8 +17,8 @@ function AboutComponent() {
             </Row>
         
       <Row>
-        <div className="d-flex flex-sm-column mx-sm-5 mx-md-2 flex-md-row justify-content-md-center  mt-3">
-        <img className="rounded-circle awar1" src={doc1} alt="award"/>
+        <div className="d-flex px-sm-5 mx-md-0 flex-md-row justify-content-md-center  mt-3">
+        <img className="rounded-circle col-xs-1 col-sm-2 col-md-4 awar1" src={doc1} alt="award"/>
         <p className="m-5 text-left w-25">We at Medicare are always fully focused on helping your to overcame any surgeon procedure with great commitment and easy recovery.</p>
         </div>
       </Row>
