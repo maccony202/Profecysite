2ecacc58df62456bae3bb4843183cdfddc6f7a37
Collins Nana Antwi <nanastef10@hotmail.com>
2023-01-21 08:15:51 +0000

    Add changes to color

--- src/Components/AboutComp.js
+++ src/Components/AboutComp.js
@@ -12,7 +12,7 @@ function AboutComponent() {
             <Row>
             <Row lg={3} sm={1} md={1} className="justify-content-lg-center mx-0 mt-5 w-100 p-3">
            <Col data-aos="fade-up" className="mt-5">
-             <h3 className="p-3 bg-danger">About Us</h3>
+             <h3 className="p-3 bg-success">About Us</h3>
              <h3>TOTAL <span className="text-danger w-100">HEALTHCARE</span> SOLUTION</h3>
              <p className="text-dark">We at Medicare are always fully focused on helping your to overcame any surgeon procedure with great commitment and easy recovery.</p>
            </Col>
@@ -29,15 +29,15 @@ function AboutComponent() {
         <img className="rounded-circle awar1" src={doc1} alt="award"/>
         <p className="w-25 mx-3 my-5">We at Medicare are always fully focused on helping your to overcame any surgeon procedure with great commitment and easy recovery.</p>
         </div>
-        <div data-aos="fade-right" className="d-flex justify-content-center">
+        <div data-aos="fade-right" className="d-flex justify-content-center pt-3">
         <img className="rounded-circle awar1" src={doc1} alt="award"/>
         <p className="w-25 mx-3 my-5">We at Medicare are always fully focused on helping your to overcame any surgeon procedure with great commitment and easy recovery.</p>
         </div>
-        <div data-aos="fade-down" className="d-flex justify-content-center">
+        <div data-aos="fade-down" className="d-flex justify-content-center pt-3">
         <img className="rounded-circle awar1" src={doc1} alt="award"/>
         <p className="w-25 mx-3 my-5">We at Medicare are always fully focused on helping your to overcame any surgeon procedure with great commitment and easy recovery.</p>
         </div>
-        <div data-aos="fade" className="d-flex justify-content-center">
+        <div data-aos="fade" className="d-flex justify-content-center pt-3">
         <img className="rounded-circle awar1" src={doc1} alt="award"/>
         <p className="w-25 mx-3 my-5">We at Medicare are always fully focused on helping your to overcame any surgeon procedure with great commitment and easy recovery.</p>
         </div>
--- src/Components/Headline.js
+++ src/Components/Headline.js
@@ -23,7 +23,7 @@ const Headline = () => {
         <Row lg={3} sm={1} md={2} className="justify-content-lg-center mx-0 mt-5 w-100 p-3">
           <img className="rounded-circle mt-5 doc" src={doc1} alt="accra"/>
            <Col data-aos="fade-up" className="mt-5">
-             <h3 className="p-3 bg-danger">HOSPITAL PROVIDING</h3>
+             <h3 className="p-3 bg-success">HOSPITAL PROVIDING</h3>
              <h3>TOTAL <span className="text-danger w-100">HEALTHCARE</span> SOLUTION</h3>
              <p className="text-dark">We at Medicare are always fully focused on helping your to overcame any surgeon procedure with great commitment and easy recovery.</p>
            </Col>
